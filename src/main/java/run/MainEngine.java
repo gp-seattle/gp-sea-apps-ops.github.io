0341fb6446d06186989d319e8755fc4939ba42ad
marquisss <32654396+marquisss@users.noreply.github.com>
2019-05-05 11:42:31 -0700

    adds event code

--- src/main/java/run/MainEngine.java
+++ src/main/java/run/MainEngine.java
@@ -14,30 +14,41 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 
+import java.util.Scanner;
+
 public class MainEngine {
-    public static void main(String[] args) {
-        CarrotEngine engine = new CarrotEngine(new Configuration.Builder()
-                .setResourceLocator(new FileResourceLocator.Builder("src/main/webapp/templates"))
-                .build());
-
-        HashMap<String, Object> values = new HashMap<String, Object>();
-        values.put("name1", "Jim");
-        values.put("name2", "Jen");
-        values.put("day1", "8/18/2019");
-        values.put("day2", "8/20/2019");
-
-        Bindings bindings = new MapBindings(values);
-
-        try {
-            String htmlOutput = engine.process("carrot_index.html", bindings);
-            // Show status
-            System.out.println("ENGINE STATUS " + htmlOutput);
-            // Output html file
-            BufferedWriter bw = new BufferedWriter(new FileWriter("index.html"));
-            bw.write(htmlOutput);
-            bw.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+   public static void main(String[] args) {
+      CarrotEngine engine = new CarrotEngine(new Configuration.Builder()
+             .setResourceLocator(new FileResourceLocator.Builder("src/main/webapp/templates"))
+             .build());
+   
+      HashMap<String, Object> values = new HashMap<String, Object>();
+      
+      File f = new File("calendar_data.csv");
+      Scanner in = new Scanner(f);
+      Set<Event> Events = new TreeSet<>();
+      in.nextLine();
+      Events.add(new Event(in.nextLine()));
+      
+      /*
+      values.put("name1", "Jim");
+      values.put("name2", "Jen");
+      values.put("day1", "8/18/2019");
+      values.put("day2", "8/20/2019");
+      */
+   
+      Bindings bindings = new MapBindings(values);
+   
+      try {
+         String htmlOutput = engine.process("carrot_index.html", bindings);
+         // Show status
+         System.out.println("ENGINE STATUS " + htmlOutput);
+         // Output html file
+         BufferedWriter bw = new BufferedWriter(new FileWriter("index.html"));
+         bw.write(htmlOutput);
+         bw.close();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
 }
